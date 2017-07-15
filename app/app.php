ccd44c8102b5ec02c3ac25a536af3c17558cd58e
Michelle Poterek <poterekm@gmail.com>
2017-07-15 12:32:19 -0700

    add basic file templates

---
+++ app/app.php
@@ -0,0 +1,26 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
+
+
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    // use Symfony\Componenet\Debug\Debug;
+    // Debug::enable();
+
+
+    $app = new Silex\Application();
+    // $app['debug'] = true;
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    // routes
+
+    return $app;
+
+?>
