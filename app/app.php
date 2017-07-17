67ef7be4f389a56f21b472daa100a325598260a4
Michelle Poterek <poterekm@gmail.com>
2017-07-16 19:11:22 -0700

    fix bug in routing to get instead of post

--- app/app.php
+++ app/app.php
@@ -3,6 +3,7 @@
     require_once __DIR__."/../src/Stylist.php";
     require_once __DIR__."/../src/Client.php";
 
+    $app = new Silex\Application();
 
     $server = 'mysql:host=localhost:8889;dbname=hair_salon';
     $username = 'root';
@@ -13,7 +14,6 @@
     // Debug::enable();
 
 
-    $app = new Silex\Application();
 
     // $app['debug'] = true;
 
@@ -22,10 +22,10 @@
     ));
 
     $app->get("/", function() use($app) {
-        return $app['twig']->render('index.html.twig', array(Stylist::getAll()));
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
     });
 
-    $app->post("/stylists", function() use ($app) {
+    $app->post("/", function() use ($app) {
         $stylist = $_POST['stylist'];
         $new_stylist = new Stylist($stylist);
 
@@ -39,18 +39,23 @@
     });
 
 ////chunk together
-    $app->post("/stylists", function() use ($app) {
-        $stylist = new Stylist($_POST['stylist']);
-        $stylist->save();
-        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
-    });
 
     $app->get("/stylists/{id}", function($id) use ($app) {
         $stylist = Stylist::find($id);
         $clients = $stylist->getClients();
-        return $app['twig']->render('index.html.twig', array('stylist' => $stylist, 'clients' => $clients));
+        return $app['twig']->render('stylists.html.twig', array('stylist' => $stylist, 'clients' => $clients));
     });
 
+
+    $app->get("/stylists", function() use ($app) {
+        return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    // $app->post("/stylists", function() use ($app) {
+    //     $stylist = new Stylist($_POST['stylist']);
+    //     $stylist->save();
+    //     return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+    // });
     return $app;
 
 ?>
